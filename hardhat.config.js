5af0886bf7a688dc7c1dff31b54e06b2f7fa4bdd
moAlobaidi <mohammed.alobaidi.2002@gmail.com>
2022-06-25 13:17:00 -0400

    seaport import lkjasdfh

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 /* hardhat.config.js */
-require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     // mumbai: {
     //   url: "https://rpc-mumbai.maticvigil.com",
@@ -13,12 +13,17 @@ module.exports = {
     // }
   },
   solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+    compilers: [
+      {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 19066,
+          },
+        },
+      },
+    ],
+  },
+};
