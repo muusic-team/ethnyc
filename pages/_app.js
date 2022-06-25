1d3cd04aa9f5c9bb93e5aa6f467b150bbf4e29e8
Daniel Gushchyan <d.gushchyan@gmail.com>
2022-06-25 15:47:17 -0400

    Starting imtegration of walletconnect

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,182 @@
 /* pages/_app.js */
-import '../styles/globals.css'
-import Link from 'next/link'
+import "../styles/globals.css";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+let providerOptions;
+
+let web3Modal;
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    providerOptions = {
+      /* See Provider Options Section */
+    };
+
+    web3Modal = new Web3Modal({
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
+  }, []);
+
+  const [provider, setProvider] = useState();
+  const [library, setLibrary] = useState();
+  const [account, setAccount] = useState();
+  const [signature, setSignature] = useState("");
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState();
+  const [network, setNetwork] = useState();
+  const [message, setMessage] = useState("");
+  const [signedMessage, setSignedMessage] = useState("");
+  const [verified, setVerified] = useState();
+
+  const connectWallet = async () => {
+    try {
+      const provider = await web3Modal.connect();
+      const library = new ethers.providers.Web3Provider(provider);
+      const accounts = await library.listAccounts();
+      const network = await library.getNetwork();
+      setProvider(provider);
+      setLibrary(library);
+      if (accounts) setAccount(accounts[0]);
+      setChainId(network.chainId);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const handleNetwork = (e) => {
+    const id = e.target.value;
+    setNetwork(Number(id));
+  };
+
+  const handleInput = (e) => {
+    const msg = e.target.value;
+    setMessage(msg);
+  };
+
+  const switchNetwork = async () => {
+    try {
+      await library.provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: toHex(network) }],
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await library.provider.request({
+            method: "wallet_addEthereumChain",
+            params: [networkParams[toHex(network)]],
+          });
+        } catch (error) {
+          setError(error);
+        }
+      }
+    }
+  };
+
+  const signMessage = async () => {
+    if (!library) return;
+    try {
+      const signature = await library.provider.request({
+        method: "personal_sign",
+        params: [message, account],
+      });
+      setSignedMessage(message);
+      setSignature(signature);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const verifyMessage = async () => {
+    if (!library) return;
+    try {
+      const verify = await library.provider.request({
+        method: "personal_ecRecover",
+        params: [signedMessage, signature],
+      });
+      setVerified(verify === account.toLowerCase());
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const refreshState = () => {
+    setAccount();
+    setChainId();
+    setNetwork("");
+    setMessage("");
+    setSignature("");
+    setVerified(undefined);
+  };
+
+  const disconnect = async () => {
+    await web3Modal.clearCachedProvider();
+    refreshState();
+  };
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      connectWallet();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts) => {
+        console.log("accountsChanged", accounts);
+        if (accounts) setAccount(accounts[0]);
+      };
+
+      const handleChainChanged = (_hexChainId) => {
+        setChainId(_hexChainId);
+      };
+
+      const handleDisconnect = () => {
+        console.log("disconnect", error);
+        disconnect();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider]);
+
   return (
-      <div>
-        <nav className="border-b p-6">
-          <p className="text-4xl font-bold">Metaverse Marketplace</p>
-          <div className="flex mt-4">
-            <Link href="/">
-              <a className="mr-4 text-pink-500">
-                Home
-              </a>
-            </Link>
-            <Link href="/create-nft">
-              <a className="mr-6 text-pink-500">
-                Sell NFT
-              </a>
-            </Link>
-            <Link href="/my-nfts">
-              <a className="mr-6 text-pink-500">
-                My NFTs
-              </a>
-            </Link>
-            <Link href="/dashboard">
-              <a className="mr-6 text-pink-500">
-                Dashboard
-              </a>
-            </Link>
-          </div>
-        </nav>
-        <Component {...pageProps} />
-      </div>
-  )
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className=""></div>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">Sell NFT</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">My NFTs</a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
