3c4e93acf8d4b47e81e5fe2f7858ef26bbf66b04
moAlobaidi <mohammed.alobaidi.2002@gmail.com>
2022-06-26 02:06:32 -0400

    yes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const marketplaceAddress = "0x9A676e781A523b5d0C0e43731313A708CB607508";
-export const conduitController = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
-export const seaportAddress = "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1";
+export const marketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const conduitController = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const seaportAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
